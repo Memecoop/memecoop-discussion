df9f07b704caaf1bdcedb934f3115542ead80550
Kevin Litwack <kevin@memecoop.com>
2011-10-20 18:52:00 +0900

    Add title and category to nodes; clean up node view & add some CSS.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110925232141) do
+ActiveRecord::Schema.define(:version => 20111020064139) do
 
   create_table "edges", :force => true do |t|
     t.string   "type"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(:version => 20110925232141) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
+    t.string   "category"
   end
 
 end
