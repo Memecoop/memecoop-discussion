6bff5b39a45aa5336891f451f76a2ef45b6725cb
Kevin Litwack <kevin@memecoop.com>
2011-11-02 17:25:56 +0900

    Allow a minimum username length of 2, instead of 4.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   acts_as_authentic do |c|
     c.login_field = :name
-    c.merge_validates_length_of_login_field_options :within => 4..50
+    c.merge_validates_length_of_login_field_options :within => 2..50
   end
 
   validates_uniqueness_of :email
