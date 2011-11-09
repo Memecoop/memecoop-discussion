afc7e38a9dc3adeba33c07ff5c37ee9bb95347b3
David Pickett <david@memecoop.com>
2011-11-09 17:39:58 +0900

    Changed name from reply_stuff to reply_form

--- app/assets/javascripts/nodes.js
+++ app/assets/javascripts/nodes.js
@@ -1,12 +1,8 @@
 $(document).ready(function(){
-  $(".reply_stuff").hide();
-  
+  $(".reply_form").hide(); 
   $(".reply_link").mousedown(function(event){
     event.preventDefault();
     $(this).prev(".reply_stuff").toggle(true);
     $(this).toggle(false);
-       
-    
   });
-
 });
