21852615540023564d70fa47ab50fdfdb8bdefb0
Kevin Litwack <kevin@memecoop.com>
2011-11-22 14:58:54 +0900

    Pushing omniauth config for memecoop live site.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,17 +1,20 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  # Certificate authority file location on Heroku
-  ca_file = '/usr/lib/ssl/certs/ca-certificates.crt'
-  # Facebook app ID for memecoop-dev
-  fb_app_id = '265123856864145'
-  # Facebook app secret for memecoop-dev
-  fb_app_secret = '91bb874df8838572395f21c90c5c70e2'
+  # Uncomment one of the following blocks, depending on which environment this instance will run in.
 
-  # Certificate authority file location on Fabric's machine
+  # Memecoop-dev
+  #ca_file = '/usr/lib/ssl/certs/ca-certificates.crt'
+  #fb_app_id = '265123856864145'
+  #fb_app_secret = '91bb874df8838572395f21c90c5c70e2'
+
+  # Memecoop
+  ca_file = '/usr/lib/ssl/certs/ca-certificates.crt'
+  fb_app_id = '233072326756470'
+  fb_app_secret = 'f72cc12399c2dafed0d05570928ab0a7'
+
+  # Local development ("127.0.0.1:3000/")
   #ca_file = "#{Rails.root}/config/ca-bundle.crt"
-  # Facebook API key for local test
   #fb_app_id = '193251400751735'
-  # Facebook app secret for local test
   #fb_app_secret = '6f5dadd17197f7c3c71d920d4fc5d1e3'
 
   provider :facebook, fb_app_id, fb_app_secret,
