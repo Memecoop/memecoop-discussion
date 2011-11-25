52add3925aae033ed6e9eb6a1f009024eb3f0b82
Kevin Litwack <kevin@memecoop.com>
2011-11-25 17:26:18 +0900

    Typo: missing closing bracket on previous checkin.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # Try to get FB Connect settings from the environment; otherwise default to local development,
   # using an app corresponding to http://127.0.0.1:3000/.
 
-  ca_file = ENV['CA_FILE' || "#{Rails.root}/config/ca-bundle.crt"
+  ca_file = ENV['CA_FILE'] || "#{Rails.root}/config/ca-bundle.crt"
   fb_app_id = ENV['FB_APP_ID'] || '193251400751735'
   fb_app_secret = ENV['FB_APP_SECRET'] || '6f5dadd17197f7c3c71d920d4fc5d1e3'
 
